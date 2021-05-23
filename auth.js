b886ed4de98de3f7b2303f813feb4be3356b545b
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2021-05-23 17:46:28 +0900

    feat: Initialize mastermind basic project

---
+++ auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const secret = 'token secret';
+const expiresIn = 365 * 24 * 3600; // 365 days
+
+const auth = {
+  signToken(id) {
+    return jwt.sign({ id }, secret, { expiresIn })
+  },
+  ensureAuth() {
+    return (req, res, next) => {
+      const { authorization } = req.headers
+      if (!authorization) {
+        res.status(401)
+        throw Error('No Authorization headers')
+      }
+
+      try {
+        req.user = this.verify(authorization)
+      } catch (e) {
+        res.status(401)
+        throw e
+      }
+
+      next()
+    }
+  },
+  verify(token) {
+    console.log("token : ", token)
+    testToken = token.replace(/^Bearer\s/, '')
+    console.log("testToken : ", testToken)
+    return jwt.verify(testToken, secret)
+  }
+}
+
+module.exports = auth
