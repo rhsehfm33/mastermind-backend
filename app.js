680ad87afd47e352a4b8612d227b81e88f55f5bf
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2021-06-11 22:56:54 +0900

    refactor: Delete useless code

--- app.js
+++ app.js
@@ -28,24 +28,6 @@ app.use('/boards', authService.ensureAuth(), boardRoute);
 app.use('/lists', authService.ensureAuth(), listRoute);
 app.use('/cards', authService.ensureAuth(), cardRoute);
 
-// app.post('/boards', authService.ensureAuth(), board.create)
-// app.get('/boards', authService.ensureAuth(), board.query)
-// app.get('/boards/:id', authService.ensureAuth(), board.get)
-// app.put('/boards/:id', authService.ensureAuth(), board.update)
-// app.delete('/boards/:id', authService.ensureAuth(), board.destroy)
-
-// app.get('/lists', authService.ensureAuth(), list.query)
-// app.get('/lists/:id', authService.ensureAuth(), list.get)
-// app.post('/lists', authService.ensureAuth(), list.create)
-// app.put('/lists/:id', authService.ensureAuth(), list.update)
-// app.delete('/lists/:id', authService.ensureAuth(), list.destroy)
-
-// app.post('/cards', authService.ensureAuth(), card.create)
-// // app.get('/cards', authService.ensureAuth(), card.query)
-// app.get('/cards/:id', authService.ensureAuth(), card.get)
-// app.put('/cards/:id', authService.ensureAuth(), card.update)
-// app.delete('/cards/:id', authService.ensureAuth(), card.destroy)
-
 const startT = Date.now();
 app.use('/health', (_, res) => res.json({ time: Date.now() - startT }));
 
